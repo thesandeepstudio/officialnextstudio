e696be39cefb4c4f960aa8f0b0fcbf1c2fa3d5fc
thesandeepstudio <thesandeep2080@gmail.com>
2024-10-23 12:17:30 +0545

    npm

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,18 @@ gulp.task('clean', function () {
 // Build task
 gulp.task('build', gulp.series('clean', function () {
     return gulp.src('src/**/*') // Change this to your actual source files
-        .pipe(plumber())
-        .pipe(exec('npm run build'))
-        .pipe(exec.reporter());
+        .pipe(plumber()) // Prevents pipe breaking caused by errors
+        .pipe(exec('npm run build')) // Runs the build command
+        .pipe(exec.reporter()); // Reports the result of the exec command
 }));
 
 // Deploy task
 gulp.task('deploy', function () {
-    return gulp.src('*')
+    return gulp.src('*') // Adjust if you want to specify a specific path for deployment
         .pipe(plumber())
-        .pipe(exec('npx wrangler publish'))
+        .pipe(exec('npx wrangler publish')) // Deploys using wrangler
         .pipe(exec.reporter());
-}));
+});
 
 // Default task: build and deploy
 gulp.task('default', gulp.series('build', 'deploy'));
