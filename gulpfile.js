b469c1fb27a93033a7572724d3a49012a88ae9b6
thesandeepstudio <thesandeep2080@gmail.com>
2024-10-23 13:55:41 +0545

    1.0.0.4

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 const gulp = require('gulp');
-const clean = require('gulp-clean'); // Make sure you have this package if using clean
-// Import other necessary plugins here, e.g., for CSS/JS processing
+const clean = require('gulp-clean');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
 
 // Clean task
 gulp.task('clean', function () {
-    return gulp.src('dist', { allowEmpty: true, read: false }) // Adjust as necessary
+    return gulp.src('dist', { allowEmpty: true, read: false })
         .pipe(clean());
 });
 
-// Example build task (update as necessary)
+// Build task
 gulp.task('build', function () {
-    return gulp.src('src/**/*') // Adjust the path as necessary
-        .pipe(gulp.dest('dist')); // Destination folder
+    // Minify and concatenate JavaScript files
+    gulp.src('src/js/**/*.js') // Path to JavaScript files
+        .pipe(concat('app.min.js')) // Concatenate files into app.min.js
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js')); // Output path for minified JS
+
+    // Minify CSS file
+    gulp.src('src/styles/styles.css') // Path to your CSS file
+        .pipe(cleanCSS()) // Minify the CSS
+        .pipe(gulp.dest('dist/styles')); // Output path for minified CSS
+
+    // Optimize images
+    gulp.src('src/image/**/*') // Path to image files (including all subfolders)
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images')); // Output path for optimized images
+
+    // Copy HTML and other files (like JSON, etc.)
+    return gulp.src('src/**/*') // Copy all files from src to dist
+        .pipe(gulp.dest('dist'));
 });
 
 // Watch task
