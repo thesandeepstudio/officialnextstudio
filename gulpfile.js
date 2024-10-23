8f49678ca6cd49804e876c140c00c166e32e48e6
thesandeepstudio <thesandeep2080@gmail.com>
2024-10-23 11:47:17 +0545

    update
    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ const exec = require('gulp-exec');
 
 // Task to build the website
 gulp.task('build', function () {
-    // You can adjust this to fit your site's build process (if needed)
+    // Adjust this to fit your actual build command (if needed)
     return gulp.src('*')
-        .pipe(exec('npm run build')); // Replace 'npm run build' with your actual build command
+        .pipe(exec('npm run build')); // Make sure 'npm run build' is defined in your package.json
 });
 
 // Task to deploy the website to Cloudflare using wrangler
 gulp.task('deploy', function () {
     return gulp.src('*')
-        .pipe(exec('wrangler publish'));
+        .pipe(exec('npx wrangler publish')); // Using npx for wrangler command
 });
 
 // Default task: build and deploy
